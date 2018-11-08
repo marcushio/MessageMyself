6acaa9967864bf6a2d99cd4ee45f401f40611250
marcushio <marcus.t.trujillo@gmail.com>
2018-11-08 14:26:12 -0700

    First activity is created

--- app/src/main/java/com/marcustrujillo/messagemyself/MainActivity.java
+++ app/src/main/java/com/marcustrujillo/messagemyself/MainActivity.java
@@ -2,12 +2,24 @@ package com.marcustrujillo.messagemyself;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+
+
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button Clicked");
     }
 }
